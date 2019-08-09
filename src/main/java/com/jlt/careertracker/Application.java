61bac2a4447e689f2ff81aac00528359098ae275
jltimm <jacobltimm@gmail.com>
2019-08-09 18:12:44 -0500

    Seed repository with values

--- src/main/java/com/jlt/careertracker/Application.java
+++ src/main/java/com/jlt/careertracker/Application.java
@@ -1,10 +1,20 @@
 package com.jlt.careertracker;
 
+import com.jlt.careertracker.models.Career;
+import com.jlt.careertracker.repositories.CareerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    /**
+     * Repository for careers so we can seed data on startup.
+     */
+    @Autowired
+    private CareerRepository careerRepository;
 
     /**
      * Entry point for application.
@@ -15,4 +25,16 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * Seed the repository with a couple values
+     *
+     * @param args The args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        careerRepository.deleteAll();
+        careerRepository.save(new Career("Software Engineer", "in progress", "Company 1"));
+        careerRepository.save(new Career("Software Engineer", "in progress", "Company 2"));
+    }
 }
